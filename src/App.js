da66451fbe4da458cd362eca45fb19c54d463807
BARATHLIN <112534630+Barathlin@users.noreply.github.com>
2023-08-11 14:16:48 +0530

    hero-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import NavBar from "./components/Navbar/navbar";
-
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (<>
 
     <NavBar/>
+    <Hero/>
   </>
  
   );
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./Hero.module.css"
+import headphone from "../../assets/headphone.png"
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+        <div className={styles.heroWrapper}>
+            <div className={styles.text}>
+            <p>100 Thousand Songs, ad-free</p>
+            <p>Over thousands podcast episodes</p>
+            </div>
+            <img src={headphone} alt="hero image" className={styles.heroImage}/>
+        </div>
+
+    </div>
+    
+  )
+}
+
+export default Hero
